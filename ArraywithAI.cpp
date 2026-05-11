88a9350ea1388c2b60cf15f91b50e17031548393
ILMfe <iqdamrifat@gmail.com>
2026-05-11 13:32:31 +0800

    Add files via upload

--- ArraywithAI.cpp
+++ ArraywithAI.cpp
@@ -1,29 +1,15 @@
 #include <iostream>
+#include <vector>
 #include <algorithm>
 using namespace std;
 
-bool isSorted(int arr[], int n) {
-    return is_sorted(arr, arr + n);
+bool isSorted(vector<int>& arr) {
+    return is_sorted(arr.begin(), arr.end());
 }
 
 int main() {
-    int arr1[] = {10, 20, 30, 40, 50};
-    int n1 = 5;
-    
-    if (isSorted(arr1, n1)) {
-        cout << "Array 1: true" << endl;
-    } else {
-        cout << "Array 1: false" << endl;
-    }
-    
-    int arr2[] = {90, 80, 100, 70, 40, 30};
-    int n2 = 6;
-    
-    if (isSorted(arr2, n2)) {
-        cout << "Array 2: true" << endl;
-    } else {
-        cout << "Array 2: false" << endl;
-    }
-    
+    vector<int> arr = {10, 20, 30, 40, 50};
+    cout << (isSorted(arr) ? "true\n" : "false\n");
+
     return 0;
 }
--- ArraywithoutAI.cpp
+++ ArraywithoutAI.cpp
@@ -1,9 +1,10 @@
 #include <iostream>
+#include <vector>
 using namespace std;
 
-bool isSorted(int arr[], int n) {
-    for (int i = 0; i < n - 1; i++) {
-        if (arr[i] > arr[i + 1]) {
+bool isSorted(vector<int>& arr) {
+    for (int i = 1; i < arr.size(); i++) {
+        if (arr[i - 1] > arr[i]) {
             return false;
         }
     }
@@ -11,23 +12,8 @@ bool isSorted(int arr[], int n) {
 }
 
 int main() {
-    int arr1[] = {10, 20, 30, 40, 50};
-    int n1 = 5;
-    
-    if (isSorted(arr1, n1)) {
-        cout << "Array 1: true" << endl;
-    } else {
-        cout << "Array 1: false" << endl;
-    }
-    
-    int arr2[] = {90, 80, 100, 70, 40, 30};
-    int n2 = 6;
-    
-    if (isSorted(arr2, n2)) {
-        cout << "Array 2: true" << endl;
-    } else {
-        cout << "Array 2: false" << endl;
-    }
-    
+    vector<int> arr = {10, 20, 30, 40, 50};
+    cout << (isSorted(arr) ? "true\n" : "false\n");
+
     return 0;
 }
